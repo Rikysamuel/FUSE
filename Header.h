b8d40ed4346fb2ecec539b7fa725caeab59911b0
Rikysamuel <rikz.samuel@gmail.com>
2014-05-08 00:49:23 +0700

    Final but not finish

--- Header.h
+++ Header.h
@@ -16,54 +16,65 @@ using std::string;
 using std::fstream;
 
 #define HEADER_SIZE 512
-#define FILE_SIZE 33686016
+#define FILE_SIZE 32000000
 
-#define ALLOC_SIZE 131072
-#define DATA_SIZE 33554432
-
-#define BLOCK_SIZE 32
-#define BLOCK_NUM 16
+#define TABLE_SIZE 32
+#define ALLOC_TABLE 65536
+#define TABLE 131072
 
 //fstream handle;
 
 class VolInfo{	
 	public:
-		uint32_t capasity;			
-		uint32_t firstEmpty;			
-		uint32_t block;	//yg blm dipake
-		time_t mount_time;		// waktu mounting, diisi di konstruktor
+		uint32_t capasity;		/* kapasitas block datapool 65536 block */
+		uint32_t firstEmpty;	/* block kosong pertama */
+		uint32_t block;			/* yang blm dipake */
+		time_t mount_time;
 	
 		/* konstruktor & destruktor */
-		VolInfo();
+		VolInfo(string volname);
 		~VolInfo();
 
-		void writeHeader();
+		void writeHeader(string volname);
+		void updateVol(char first);
 };
 
 class AllocTable{
 	public:
+		unsigned short address;
+
 		AllocTable();
 		~AllocTable();
-		void isEmpty();
-		void setEmpty();
-		void setNext();
-
+		bool isEmpty();
+		void setAddress(unsigned short _addr);	//set address
+		unsigned short getAddress();
+		void setEmpty();	
+		void write();
 };
 
 class DataPool{
-	private:
-		char blocksize[BLOCK_SIZE];
-
 	public:
+		char attribute;
+
 		DataPool();
 		~DataPool();
-		void InitRoot();
+		void createBlock();
+		void createEntry();
+		char * readFile(char * buffer, int begin, int number);
+		bool isDirectory();
+		void setName(int block, int entry, string name);
+		char getAttr();
+		unsigned short MakeTime();
+		unsigned short MakeDate();
+		unsigned short setPointer();
+		int size();
 };
 
 class Controller{
 	public:
-		Controller();
+		Controller(string filename);
 		~Controller();
+		void setAddress(char * buffer, AllocTable A, int position, unsigned short address);
 };
 
 #endif
